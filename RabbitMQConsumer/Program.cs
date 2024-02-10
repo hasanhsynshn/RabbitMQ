2db1f4bae191b0d63415dea84b40014405f6ffcf
Hasan Hüseyin Sahin <hasanhsynshn7@gmail.com>
2024-02-10 17:00:11 +0300

    consumer added

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Can be read from appsettings or somewhere like etcd
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "info", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Incoming message: {0}", message);
+    };
+    channel.BasicConsume(queue: "info", autoAck: true, consumer: consumer);
+}
