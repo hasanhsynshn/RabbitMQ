2d73434f494eb4fb3b737e95b522eb20b5798f6a
Hasan Hüseyin Sahin <hasanhsynshn7@gmail.com>
2024-02-10 17:11:23 +0300

    directory issues solved

---
+++ RabbitMQPublisher/MessageModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace RabbitMQPublisher
+{
+    public class MessageModel
+    {
+        public string MessageId { get; set; } = "test";
+        public string Sender { get; set; } = "hasan hüseyin";
+        public string Recipient { get; set; } = "consumer";
+        public string Content { get; set; } = "first message";
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQPublisher;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare("test_exchange",ExchangeType.Direct,false,false);
+    channel.QueueDeclare(queue: "info", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    channel.QueueBind("info","test_exchange","info_route");
+    // Message content
+    string message = new MessageModel().ToString();
+    var body = Encoding.UTF8.GetBytes(message);
+
+    
+    channel.BasicPublish(exchange: "direct",
+                         routingKey: "info", // Routing key
+                         basicProperties: null,
+                         body: body);
+}
+
